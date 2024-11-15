dee6d10e59680c1d2bf9e956057285c4366824ad
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-15 14:07:02 +0100

    add markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
+import languages from "./db/languages.js";
 
 function App() {
 
 
   return (
     <>
+
+      <div className="container">
+
+        <div className="tabs">
+          <ul>
+
+            {languages.map((item, index) => {
+              return (
+                <li key={item.id}>
+                  <div className="title">
+                    <span>{item.title}</span>
+                  </div>
+                  <div className="description">
+                    <p>{item.description}</p>
+                  </div>
+                </li>
+              );
+            })}
+
+          </ul>
+        </div>
+      </div>
 
     </>
   )
