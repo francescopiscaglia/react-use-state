3b8cb7e4fb20a665947a3f5c288c960b085219a4
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-15 15:26:20 +0100

    add map and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import './App.css'
+
+import './App.css';
 import languages from "./db/languages.js";
 
 function App() {
 
+  // logic
 
+
+
+  // render
   return (
-    <>
+    <div className="container">
 
-      <div className="container">
+      <h1>Learn Web Development</h1>
 
-        <div className="tabs">
-          <ul>
+      <div className="tabs">
 
-            {languages.map((item, index) => {
-              return (
-                <li key={item.id}>
-                  <div className="title">
-                    <span>{item.title}</span>
-                  </div>
-                  <div className="description">
-                    <p>{item.description}</p>
-                  </div>
-                </li>
-              );
-            })}
-
-          </ul>
-        </div>
+        {/* map per avere i bottoni in pagina */}
+        {languages.map((item, index) => (
+          <button key={item.id}>
+            {item.title}
+          </button>
+        ))}
       </div>
 
-    </>
-  )
+
+      {/* map per avere le description in pagina */}
+      {languages.map((item, index) => (
+
+        // se active corrisponde ad index imposto la classe active se no hide
+        <div className='tab-content' key={item.id}>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+        </div>
+      )
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
